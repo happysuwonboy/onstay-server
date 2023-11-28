99ff61e58d0b3a0f31d3c8760663aefb406387c0
yunjeong <yjleeeeee6@gmail.com>
2023-11-28 14:48:03 +0900

    [Add] NewAccContent 서버 구현
    - NewStaySection 경로 임시 수정
    - 등록일과 현재일 차이값 요청
    - 최소인원과 최대인원 요청

--- controller/newStayController.js
+++ controller/newStayController.js
@@ -1,6 +1,21 @@
 import * as newStayRepostory from '../repository/newStayRepository.js';
 
+/**
+ * NewStaySection
+ * @param {*} req 
+ * @param {*} res 
+ */
 export async function getList(req, res) {
   const result = await newStayRepostory.getList();
   res.json(result);
 };
+
+/**
+ * NewAccContent
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function getAccList(req, res) {
+  const result = await newStayRepostory.getAccList();
+  res.json(result);
+}
--- repository/newStayRepository.js
+++ repository/newStayRepository.js
@@ -1,5 +1,9 @@
 import { db } from '../db/database.js';
 
+/**
+ * NewStaySection
+ * @returns 3개월 이내 등록된 숙소 데이터와 이미지
+ */
 export async function getList() {
   const sql = `
   select acc.acc_id, 
@@ -16,3 +20,25 @@ export async function getList() {
     .execute(sql)
     .then(row => row[0])
 };
+
+/**
+ * NewAccContent
+ * @returns 3개월 이내 등록된 숙소 데이터와 객실 데이터, datediff 값 응답
+ */
+export async function getAccList() {
+  const sql = `
+  select acc.acc_id, 
+  acc.acc_name,
+  acc.register_date, 
+  room.min_capa, 
+  room.max_capa, 
+  datediff(curdate(), 
+  acc.register_date) as day_diff
+  from accommodation acc, room
+  where acc.acc_id = room.acc_id
+  and register_date between date_add(now(), interval -3 month) and now()
+  and room_name = '안채';`
+  return db
+    .execute(sql)
+    .then(row => row[0])
+}

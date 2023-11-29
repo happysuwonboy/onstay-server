42e457073762fbffcd10902e063e66be6e1bf15b
jeongyumin <onewmean@gmail.com>
2023-11-29 12:19:43 +0900

    [ Add ] roomdetail 서버 구현 (ing)
    - getRoomDate
      해당하는 숙소의 오늘 날짜 이후 체크인, 체크아웃 예약 리스트 조회
    
      roomRouter.js, roomController.js, roomRepository.js

--- controller/roomController.js
+++ controller/roomController.js
@@ -8,6 +8,18 @@ import * as roomRepository from '../repository/roomRepository.js';
 export async function getAccRoom(req, res) {
   const roomid = req.params.roomid;
 
-  const row = await roomRepository.getAccRoom(roomid);
-  res.json(row);
+  const rows = await roomRepository.getAccRoom(roomid);
+  res.json(rows);
+}
+
+/**
+ * getRoomDate : 해당하는 숙소의 오늘 날짜 이후 체크인, 체크아웃 예약 리스트 조회
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function getRoomDate(req, res) {
+  const roomid = req.params.roomid;
+
+  const rows = await roomRepository.getRoomDate(roomid);
+  res.json(rows);
 }
--- router/roomRouter.js
+++ router/roomRouter.js
@@ -3,5 +3,6 @@ import * as roomController from '../controller/roomController.js';
 
 const router = express();
 router.get('/:roomid', roomController.getAccRoom);
+router.get('/date/:roomid', roomController.getRoomDate);
 
 export default router;

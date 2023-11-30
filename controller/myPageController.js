32dc702201477d6cd29a40504b8f3f41de478269
syh <yhoonsong96@gmail.com>
2023-12-01 01:37:28 +0900

    [Add] 마이페이지 예약내역 조회 api 구현

--- controller/myPageController.js
+++ controller/myPageController.js
@@ -1,9 +1,19 @@
 import * as myPageRepository from '../repository/myPageRepository.js';
+import * as accRepository from '../repository/accRepository.js';
 
 
 export async function getUserReservation(req,res) {
   const user_id = req.params.user_id;
-  const result = await myPageRepository.getUserReservation(user_id)
-  if (result === 'no result') return res.status(400).send('no result')
-  res.status(200).send({result})
+  const reservsations = await myPageRepository.getUserReservation(user_id);
+
+  if (reservsations === 'no result') return res.status(200).send([])
+
+  const result = [];
+  
+  for (const reservation of reservsations) {
+    const images = await accRepository.getAccImages(reservation.acc_id);
+    result.push({...reservation, images})
+  }
+
+  res.status(200).send(result)
 }
--- repository/myPageRepository.js
+++ repository/myPageRepository.js
@@ -4,10 +4,11 @@ import { db } from '../db/database.js';
 export async function getUserReservation(user_id) {
   return db
     .execute(`select reservation_id, acc_name, rm.acc_id, room_name,
-            pay_date, checkin, checkout, room_price  
-            from reservation rs inner join room rm inner join accommodation ac
-            on rs.room_id = rm.room_id and rm.acc_id = ac.acc_id
-            where user_id = ? order by checkin asc`, [user_id])
+                pay_date, checkin, checkout, room_price,
+                acc_checkin checkin_time, acc_checkout checkout_time
+              from reservation rs inner join room rm inner join accommodation ac
+              on rs.room_id = rm.room_id and rm.acc_id = ac.acc_id
+               where user_id = ? order by checkin asc`, [user_id])
     .then(result => result[0].length ? result[0] : 'no result')
     .catch(err => console.log(err))
 }

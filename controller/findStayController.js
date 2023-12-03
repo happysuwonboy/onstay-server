12185e6948dd22fa3b84f8878d169d21ec750845
tneod2 <tneod2@icloud.com>
2023-12-04 08:46:48 +0900

    [Add] FindStay 필터 기능 sql수정

--- controller/findStayController.js
+++ controller/findStayController.js
@@ -1,10 +1,10 @@
 import * as findStayRepository from '../repository/findStayRepository.js';
 
 export async function getAccList(req,res) {
-    const { personnel } = req.query;
+    const { personnel, minPrice, maxPrice, sort, checkin, checkout} = req.query;
 
     try {
-        const result = await findStayRepository.getAccList({ personnel });
+        const result = await findStayRepository.getAccList({ personnel, minPrice, maxPrice, sort, checkin, checkout});
         res.json(result);
     } catch (error) {
         console.error('DB에서 숙소리스트 가져오는 중 에러 발생 => ' + error);

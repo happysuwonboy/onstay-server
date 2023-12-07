8c795aede5f3967ee05997dbbd3dbe44a040e87f
jeongyumin <onewmean@gmail.com>
2023-12-07 17:38:10 +0900

    [ Add ] roomdetail 페이지 안내사항에 따른 조건 추가
    - roomRepository.js
      getAccRoom : pet 조건 출력 추가

--- repository/roomRepository.js
+++ repository/roomRepository.js
@@ -6,12 +6,12 @@ import {db} from '../db/database.js';
  * @returns rows 데이터
  */
 export async function getAccRoom(roomId) {
-  const sql = `select ac.acc_id, ac.acc_name, ac.acc_summary1, acc_checkin, acc_checkout, 
+  const sql = `select ac.acc_id, ac.acc_name, ac.acc_summary1, acc_checkin, acc_checkout, ac.pet,
                       rm.room_id, rm.room_name, rm.room_price, rm.feature_codes, rm.amenities,  
                       rm.min_capa, rm.max_capa, rm.room_img1, rm.room_img2, rm.room_img3
-                  from accommodation ac, room rm 
-                    where ac.acc_id = rm.acc_id
-                    and rm.acc_id in (select acc_id from room where room_id = ?)`;
+                from accommodation ac, room rm 
+                where ac.acc_id = rm.acc_id
+                and rm.acc_id in (select acc_id from room where room_id = ?);`
 
   return db
     .execute(sql, [roomId]) 

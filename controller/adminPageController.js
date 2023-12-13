0ee18cd52043e60a5a366b92732f7521a86bda89
최유진 <tneod2@icloud.com>
2023-12-13 13:52:42 +0900

    [Add] 관리자페이지 숙소 관리 작업중

---
+++ controller/adminPageController.js
@@ -0,0 +1,11 @@
+import * as adminPageRepository from '../repository/adminPageRepository.js';
+
+/* 숙소리스트 조회 */
+export async function getAccList(req,res) {
+    try {
+        const result = await adminPageRepository.getAccList();
+        res.json(result);
+    } catch (error) {
+        console.error('DB에서 숙소리스트 가져오는 중 에러 발생 => ' + error);
+    }
+}
---
+++ repository/adminPageRepository.js
@@ -0,0 +1,31 @@
+import {db} from '../db/database.js';
+
+/* 숙소 리스트 조회 */
+export async function getAccList() {
+    const sql = `SELECT 
+                    acc.acc_name, 
+                    acc.register_date,
+                    acc.area_code,
+                    rm.room_name, 
+                    FORMAT(rm.room_price,0) as room_price, 
+                    rm.room_img1
+                FROM 
+                    accommodation acc, room rm
+                WHERE acc.acc_id = rm.acc_id;
+                `;
+    
+                // `
+                // SELECT *
+                // FROM (
+                //     SELECT acc.acc_name, rm.room_name, rm.room_price,  acc_img.acc_img,
+                //         ROW_NUMBER() OVER (PARTITION BY rm.room_id) as row_num
+                //     FROM 
+                //         accommodation acc, room rm
+                //     WHERE room rm ON acc.acc_id = rm.acc_id
+                // ) AS accs
+                // WHERE row_num = 1;
+                // `;
+    return db
+    .execute(sql)
+    .then((rows) => rows[0]);
+}

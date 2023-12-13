003f1110a4eb2f0404abc9bc1bfa817161d632c7
jeongyumin <onewmean@gmail.com>
2023-12-13 10:54:31 +0900

    [ Add ] roomdetail 페이지 리뷰 관련 요청 구현
    - roomRouter, roomController, roomRepository.js
    
      getReview
      : 해당하는 숙소의 리뷰 리스트와 user_name 조회
    
      getReservation
      : 해당 회원, 해당 객실의 체크아웃 이후 예약 리스트 조회
      ( 리뷰 등록 가능 기준 )
    
    - categoryRepository.js
      주석 설명 수정

--- controller/roomController.js
+++ controller/roomController.js
@@ -19,7 +19,28 @@ export async function getAccRoom(req, res) {
  */
 export async function getRoomDate(req, res) {
   const roomId = req.params.roomid;
-
   const rows = await roomRepository.getRoomDate(roomId);
+  res.json(rows);
+}
+
+/**
+ * getReview: 해당하는 숙소의 리뷰 리스트와 user_name 조회
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function getReview(req, res) {
+  const roomId = req.params.roomid;
+  const rows = await roomRepository.getReview(roomId);
+  res.json(rows);
+}
+
+/**
+ * getReservation : 해당 회원, 해당 객실의 체크아웃 이후 예약 리스트 조회 ( 리뷰 등록 가능 기준 )
+ * @param {*} req 
+ * @param {*} res 
+ */
+export async function getReservation(req, res) {
+  const {roomid, userid} = req.params;
+  const rows = await roomRepository.getReservation(roomid, userid);
   res.json(rows);
 }
--- router/roomRouter.js
+++ router/roomRouter.js
@@ -4,5 +4,7 @@ import * as roomController from '../controller/roomController.js';
 const router = express();
 router.get('/:roomid', roomController.getAccRoom);
 router.get('/date/:roomid', roomController.getRoomDate);
+router.get('/review/:roomid', roomController.getReview);
+router.get('/:roomid/:userid', roomController.getReservation);
 
 export default router;

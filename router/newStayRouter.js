392b88ca5aa65305837c4c13eee07988553710e4
yunjeong <yjleeeeee6@gmail.com>
2023-12-19 10:09:37 +0900

    [Modify] NewStay 일부 수정

--- router/newStayRouter.js
+++ router/newStayRouter.js
@@ -4,7 +4,7 @@ import * as newStayController from '../controller/newStayController.js';
 const router = express.Router();
 
 router.get('/home', newStayController.getList);
-router.get('/today/:page', newStayController.getTodayAcc);
+router.get('/today', newStayController.getTodayAcc);
 router.post('/today/coupon', newStayController.addCoupon);
 router.post('/', newStayController.getAccList);
 

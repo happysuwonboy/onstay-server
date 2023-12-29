44b004c8f70604cf47af4e7bc70ef0544255c9fc
DESKTOP-H6B44QU\TJ <yhoonsong96@gmail.com>
2023-12-29 13:16:07 +0900

    [Implement] Member - 액세스 토큰 저장 방식 변경
    - 액세스토큰 : 기존 쿠키 저장 방식에서 메모리 레벨에 저장하도록 변경
                    (Context Api 활용 -> LoginContext 추가)
      리프레쉬 토큰 : 기존 방식 유지 (httponly 쿠키)
    
    - 위 변경 사항에 따라 일부 코드 수정
      - 토큰 체크 api의 요청 메서드를 post로 수정
      - accesstoken 값을 요청 헤더의 authorization 으로 받도록 수정
      - accesstoken 을 더 이상 쿠키로 보내지 않도록 수정하였으며, 응답 데이터로
        전송하여 프론트에서 메모리레벨에 저장할 수 있도록 변경

--- router/memberRouter.js
+++ router/memberRouter.js
@@ -18,7 +18,7 @@ router.post('/login', memberController.userLogin)
 router.post('/logout', memberController.userLogout)
 
 /** 액세스 토큰 및 리프레쉬 토큰 체크 */
-router.get('/tokenCheck', memberController.tokenCheck)
+router.post('/tokenCheck', memberController.tokenCheck)
 
 /** 유저 정보 조회 */
 router.get('/userinfo/:user_id', memberController.getUserInfo);
